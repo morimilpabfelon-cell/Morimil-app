c1ccec7c4b7a92d47c2382ad7834a03cb8eebf4c
morimilpabfelon-cell <morimil.pabfelon@gmail.com>
2026-07-02 17:37:26 -0500

    Add runtime note composable

---
+++ app/src/main/java/com/morimil/app/ui/RuntimeNote.kt
@@ -0,0 +1,9 @@
+package com.morimil.app.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RuntimeNote() {
+    Text("Configurable runtime")
+}
