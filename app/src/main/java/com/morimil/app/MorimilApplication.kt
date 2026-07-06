57923227612e245b214b0bb313d7c95602181bf9
morimilpabfelon-cell <morimil.pabfelon@gmail.com>
2026-07-06 10:14:35 -0500

    Install native browser runtime

--- app/src/main/java/com/morimil/app/MorimilApplication.kt
+++ app/src/main/java/com/morimil/app/MorimilApplication.kt
@@ -1,8 +1,14 @@
 package com.morimil.app
 
 import android.app.Application
+import com.morimil.app.net.NativeBrowserRuntime
 
 class MorimilApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        NativeBrowserRuntime.install(this)
+    }
+
     val container: MorimilAppContainer by lazy {
         MorimilAppContainer(this)
     }
