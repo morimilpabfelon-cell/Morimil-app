9a037958661642ccb067cbe06cbd324b24438c14
morimilpabfelon-cell <morimil.pabfelon@gmail.com>
2026-07-05 22:52:53 -0500

    Cover stronger project intent cases

--- app/src/test/java/com/morimil/app/core/project/ProjectIntentDetectorTest.kt
+++ app/src/test/java/com/morimil/app/core/project/ProjectIntentDetectorTest.kt
@@ -22,8 +22,22 @@ class ProjectIntentDetectorTest {
     }
 
     @Test
-    fun ignoresCasualProjectDiscussion() {
+    fun detectsExistingCompanyDevelopmentLanguage() {
+        val intent = ProjectIntentDetector.detect("hola estoy desarrollando una empresa de tecnologia llamada ABITS-137 Technologies")
+
+        assertEquals("ABITS-137 Technologies", intent?.displayName)
+    }
+
+    @Test
+    fun ignoresExplicitNotYetLanguage() {
         val intent = ProjectIntentDetector.detect("estamos hablando de una empresa pero todavia no la vamos a crear")
+
+        assertNull(intent)
+    }
+
+    @Test
+    fun ignoresCasualProjectDiscussion() {
+        val intent = ProjectIntentDetector.detect("estamos hablando de una empresa interesante para algun dia")
 
         assertNull(intent)
     }
