c75f3a9a993e04b6c28a1ca5b76ee3f58895f08b
morimilpabfelon-cell <morimil.pabfelon@gmail.com>
2026-07-07 15:43:20 -0500

    Fix desktop browser viewport weight import

--- app/src/main/java/com/morimil/app/ui/NativeWebBridgePanel.kt
+++ app/src/main/java/com/morimil/app/ui/NativeWebBridgePanel.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
